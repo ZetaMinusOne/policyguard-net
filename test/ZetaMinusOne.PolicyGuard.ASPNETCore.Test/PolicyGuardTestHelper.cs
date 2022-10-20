f22f92788170913f5a012582f62a260404820431
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-20 17:03:36 -0400

    PolicyGuardTestHelp class added.
    Added methods to fake a request to the PolicyGuard API and HttpResponses

---
+++ test/ZetaMinusOne.PolicyGuard.ASPNETCore.Test/PolicyGuardTestHelper.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using Moq.Protected;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace ZetaMinusOne.PolicyGuard.ASPNETCore.Test
+{
+    public class PolicyGuardTestHelper
+    {
+        public static Mock<HttpMessageHandler> FakeMessageHandler(HttpResponseMessage mockedResponse)
+        {
+            Mock<HttpMessageHandler> mockHandler = new();
+            mockHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync",
+                ItExpr.Is<HttpRequestMessage>(r => r.Method == HttpMethod.Get),
+                ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(mockedResponse);
+
+            return mockHandler;
+        }
+
+        public static HttpResponseMessage FakeHttpResponse(PolicyHeaders headers, HttpStatusCode statusCode)
+        {
+            string json = JsonConvert.SerializeObject(headers);
+            HttpResponseMessage httpResponse = new();
+            httpResponse.StatusCode = HttpStatusCode.NotFound;
+            httpResponse.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            return httpResponse;
+        }
+
+    }
+}
