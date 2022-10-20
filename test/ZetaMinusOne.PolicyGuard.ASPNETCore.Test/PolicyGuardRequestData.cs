9531876c528330ab4dfb67937d91bddb377eb355
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-20 18:00:25 -0400

    Add a class containing Request data to pass on the test method

---
+++ test/ZetaMinusOne.PolicyGuard.ASPNETCore.Test/PolicyGuardRequestData.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+
+namespace ZetaMinusOne.PolicyGuard.ASPNETCore.Test
+{
+    public class PolicyGuardRequestData 
+    {
+        public static IEnumerable<object[]> GetRequest()
+        {
+            return new List<object[]>
+            {
+                // data with invalid apikey
+                new object []
+                {
+                    new PolicyHeaders(),
+                    "",
+                    HttpStatusCode.InternalServerError
+                },
+                new object []
+                {
+                    new PolicyHeaders(),
+                    "123432",
+                    HttpStatusCode.InternalServerError
+                },
+                new object []
+                {
+                    new PolicyHeaders(),
+                    "2131230912830918318",
+                    HttpStatusCode.InternalServerError
+                },
+
+                // data with "valid" apikey
+                new object []
+                {
+                    new PolicyHeaders {
+                        {"header1", "value1" },
+                    },
+                    "1234567890",
+                    HttpStatusCode.OK
+                },
+                new object []
+                {
+                    new PolicyHeaders {
+                        {"header1", "value1" },
+                        {"header2", "value2" },
+                    },
+                    "12345690",
+                    HttpStatusCode.OK
+                },
+                new object []
+                {
+                    new PolicyHeaders {
+                        {"header1", "value1" },
+                        {"header2", "value2" },
+                        {"header3", "value3" },
+                    },
+                    "14560",
+                    HttpStatusCode.OK
+                },
+            };
+        }
+    }
+}
