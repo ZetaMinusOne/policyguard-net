addb982d132f629b2a1735670e3d40174a225d85
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-21 11:50:14 -0400

    Added PolicyGuardMiddlewareExtension class
    This is an extension class for expose the PolicyGuardMiddleware

---
+++ libraries/ZetaMinusOne.PolicyGuard.ASPNETCore/PolicyGuardMiddlewareExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ZetaMinusOne.PolicyGuard.ASPNETCore
+{
+    public static class PolicyGuardMiddlewareExtensions
+    { 
+        public static IApplicationBuilder UsePolicyGuardHeaders(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<PolicyGuardMiddleware>();
+        }
+    }
+
+}
