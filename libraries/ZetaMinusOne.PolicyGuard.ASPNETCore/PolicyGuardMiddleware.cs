4f93a88f4ba21a891f4d9d37f4e06efaffa34442
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-21 11:13:49 -0400

    Add PolicyGuardMiddleware class

---
+++ libraries/ZetaMinusOne.PolicyGuard.ASPNETCore/PolicyGuardMiddleware.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ZetaMinusOne.PolicyGuard.ASPNETCore
+{
+    public static class PolicyGuardMiddleware
+    { 
+        public static IApplicationBuilder UsePolicyGuard(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<PolicyGuard>();
+        }
+    }
+
+}
