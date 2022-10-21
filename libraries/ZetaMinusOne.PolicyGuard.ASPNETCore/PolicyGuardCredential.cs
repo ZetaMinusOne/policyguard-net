c83961c87dedb708b887fddd20c38c95ee14bb92
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-21 16:57:35 -0400

    Move PolicyGuardCredential to a separate file

---
+++ libraries/ZetaMinusOne.PolicyGuard.ASPNETCore/PolicyGuardCredential.cs
@@ -0,0 +1,16 @@
+﻿namespace ZetaMinusOne.PolicyGuard.ASPNETCore
+{
+    /// <summary>
+    /// Class for storing the ApiKey of your 
+    /// Registered Domain in <seealso href="https://www.policyguard.io/"/>
+    /// </summary>
+    public class PolicyGuardCredential
+    {
+        /// <summary>
+        /// Assigned Api Key for your Domain.
+        /// Visit <seealso href="https://www.policyguard.io/"/> 
+        /// to optain your Api Key
+        /// </summary>
+        public string ApiKey { get; set; } = string.Empty;
+    }
+}
