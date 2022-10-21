22efcdee7fcc1bdbbbfc61aad4c5886add7cbd40
Jean Carlo Semprit Rodriguez <jc.semprit@zetaminusone.com>
2022-10-21 15:05:19 -0400

    Add Update_the_policy_headers test case

--- test/ZetaMinusOne.PolicyGuard.ASPNETCore.Test/PolicyGuardTest.cs
+++ test/ZetaMinusOne.PolicyGuard.ASPNETCore.Test/PolicyGuardTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Http;
 using Moq;
 using System.Net;
 
@@ -28,17 +29,29 @@ namespace ZetaMinusOne.PolicyGuard.ASPNETCore.Test
             Assert.Equal(expectedHeaders, actualHeaders);
         }
 
-        //[Fact]
-        //public async Task Update_the_policy_headers()
-        //{
-        //    // Arrange
-        //    var httpClient = new HttpClient();
-        //    var sut = new PolicyGuard(httpClient);
+        [Theory]
+        [MemberData(
+            nameof(PolicyGuardRequestData.GetRequest),
+            MemberType = typeof(PolicyGuardRequestData))]
+        public async Task Update_the_policy_headers(
+            PolicyHeaders expectedHeaders, string apiKey, HttpStatusCode statusCode)
+        {
+            // Arrange
+            var httpResponse =
+                PolicyGuardTestHelper.FakeHttpResponse(expectedHeaders, statusCode);
+            Mock<HttpMessageHandler> mockHandler =
+                PolicyGuardTestHelper.FakeMessageHandler(httpResponse);
+            HttpClient httpClient = new(mockHandler.Object);
+            var sut = new PolicyGuard(httpClient, apiKey);
 
-        //    // Act
+            // Act
+            var context =
+                await sut.SetPolicyGuardHeaders(new DefaultHttpContext());
+            var actualHeaders = context.Response.Headers.ToDictionary(
+                x => x.Key, x=> x.Value.ToString());
 
-        //    // Assert
-
-        //}
+            // Assert
+            Assert.Equal(expectedHeaders, actualHeaders);
+        }
     }
 }
